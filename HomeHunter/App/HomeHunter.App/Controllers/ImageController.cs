c6e421747553456534eaff07a4a665f101ec8739
CryptoEcliptic <r.e.vassilev@gmail.com>
2021-01-31 21:23:56 +0200

    Small UI changes. Added RequestFormLimits attribute in the image controller

--- HomeHunter/App/HomeHunter.App/Controllers/ImageController.cs
+++ HomeHunter/App/HomeHunter.App/Controllers/ImageController.cs
@@ -40,6 +40,7 @@ namespace HomeHunter.App.Controllers
         }
 
         [HttpPost]
+        [RequestFormLimits(MultipartBodyLengthLimit = 104857600)]
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> Upload(string id, ImageUploadBindingModel model)
         {
